ede7640884211ba659d69bff4ba6f22742224e05
johanjimenex <johan.jimenez.23@gmail.com>
2024-03-22 01:00:50 -0400

    feat: :sparkles: add new move go down or super power

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,21 +10,57 @@ public class PlayerMovement : MonoBehaviour {
 
     [HideInInspector] public Action<float> OnPositionYChanged { get; set; }
 
-
     void Update() {
         Move();
+        EmitPlayerPositionY();
+        DeadIfPlayerFalls();
     }
 
+    private Vector2 startTouchPosition, endTouchPosition;
+
     private void Move() {
-        if (Input.touchCount > 0 || Input.GetButtonDown("Jump")) {
+
+        if (Input.GetButtonDown("Jump")) {
             rb.velocity = Vector2.up * upForce;
         }
 
-        OnPositionYChanged?.Invoke(transform.position.y);
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            startTouchPosition = Input.GetTouch(0).position;
+        }
 
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+
+            endTouchPosition = Input.GetTouch(0).position;
+
+            if (endTouchPosition.y < startTouchPosition.y && ) {
+                rb.velocity = Vector2.down * upForce;
+                Invoke(nameof(StopDownForce), 0.3f);
+            }
+            else {
+                rb.velocity = Vector2.up * upForce;
+            }
+        }
+    }
+
+    private void StopDownForce() {
+        // rb.velocity = Vector2.up * 3;
+    }
+
+    private void EmitPlayerPositionY() {
+        OnPositionYChanged?.Invoke(transform.position.y);
+    }
+
+    private void DeadIfPlayerFalls() {
         if (transform.position.y > 20) {
             Destroy(ground);
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D other) {
+
+        if (other.gameObject.TryGetComponent(out IMakeDamage makeDamage) && other.transform.position.y < transform.position.y) {
+            rb.velocity = Vector2.up * upForce;
+        }
+    }
 }
+
