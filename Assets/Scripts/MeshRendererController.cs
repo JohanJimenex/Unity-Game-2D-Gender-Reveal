be71ddf3879d66a1514e08baae2acb8e62cb0b56
johanjimenex <johan.jimenez.23@gmail.com>
2024-04-15 22:52:16 -0400

    feat: :sparkles: add dinamic planets transform with camera

--- Assets/Scripts/MeshRendererController.cs
+++ Assets/Scripts/MeshRendererController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class MeshRendererController : MonoBehaviour {
 
+    [SerializeField] private Transform cameraTransform;
     [SerializeField] private Vector2 offset = new Vector2(0, 0);
     [SerializeField] private MeshRenderer meshRenderer;
 
-    void Update() {
+    private void LateUpdate() {
+        transform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y, transform.position.z);
+    }
+
+    private void Update() {
         meshRenderer.material.mainTextureOffset = new Vector2(offset.x, offset.y) * Time.time;
     }
+
 }
