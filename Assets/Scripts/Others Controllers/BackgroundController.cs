8fbd1d22bafab67d7e23ba07e52c1f5d82037dd5
johanjimenex <johan.jimenez.23@gmail.com>
2024-04-20 19:40:35 -0400

    fix: :bug: handle position by screen limits

--- Assets/Scripts/Others Controllers/BackgroundController.cs
+++ Assets/Scripts/Others Controllers/BackgroundController.cs
@@ -4,19 +4,57 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour {
 
+    [Header("Dependecies")]
+    [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private Transform cameraTransform;
+
+    [Header("Background Speed")]
     [SerializeField] private float verticalSpeed = -0.10f;
     [SerializeField] private float horizontalSpeed = -0.10f;
-
     [SerializeField] private float extraVerticalSpeed = 0.10f;
 
-    [SerializeField] private PlayerMovement playerMovement;
+    [Header("Screen Limits")]
+    [SerializeField] private float maxLimitY = 16;
+    [SerializeField] private float maxLimitX = 7;
+
+    [Header("Re-positions points")]
+    [SerializeField] private float repositionY = 16;
+    [SerializeField] private float repositionX = 7;
 
     private float initialVerticalSpeed;
 
     private void Start() {
         playerMovement.OnPositionYChanged += AddExtraVerticalSpeed;
         initialVerticalSpeed = verticalSpeed;
+    }
+
+    private void Update() {
+        TranslateBackgroundPosition();
+        HandleScreenLimits();
+    }
+
+    private void TranslateBackgroundPosition() {
+        transform.Translate(new Vector3(horizontalSpeed, verticalSpeed, transform.position.z) * Time.deltaTime);
+    }
+
+    private void HandleScreenLimits() {
+
+        if (transform.position.x < -maxLimitX) {
+            transform.position = new Vector3(repositionX, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > maxLimitX) {
+            transform.position = new Vector3(-repositionX, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y < -maxLimitY) {
+            transform.position = new Vector3(transform.position.x, repositionY, transform.position.z);
+        }
+
+        if (transform.position.y > maxLimitY) {
+            transform.position = new Vector3(transform.position.x, -repositionY, transform.position.z);
+        }
+
     }
 
     private float tempPositionY = 0;
@@ -33,29 +71,5 @@ public class BackgroundController : MonoBehaviour {
 
     }
 
-    private void LateUpdate() {
-        TranslateBackgroundPosition();
-    }
-
-    private void TranslateBackgroundPosition() {
-
-        transform.Translate(new Vector3(horizontalSpeed, verticalSpeed, transform.position.z) * Time.deltaTime);
-
-        if (transform.position.y <= -16) {
-            RepositionYBackground();
-        }
-
-        if (transform.position.x <= -7) {
-            RepositionXBackground();
-        }
-    }
-
-    private void RepositionYBackground() {
-        transform.position = new Vector3(cameraTransform.position.x, 16, transform.position.z);
-    }
-
-    private void RepositionXBackground() {
-        transform.position = new Vector3(7, transform.position.y, transform.position.z);
-    }
 
 }
