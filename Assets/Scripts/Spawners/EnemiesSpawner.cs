cd8f5b09d705906b976882afa7d2e827c7b699a9
johanjimenex <johan.jimenez.23@gmail.com>
2024-04-22 07:22:24 -0400

    fix: enemies spawner frecuency

--- Assets/Scripts/Spawners/EnemiesSpawner.cs
+++ Assets/Scripts/Spawners/EnemiesSpawner.cs
@@ -5,19 +5,18 @@ using UnityEngine;
 
 public class EnemiesSpawner : MonoBehaviour {
 
-
+    [Header("Dependencies")]
+    [SerializeField] private Transform playerTransform;
     [SerializeField] private GameObject[] easyEnemiesPrefabs;
     [SerializeField] private GameObject[] hardEnemiesPrefabs;
 
-    [SerializeField] private Transform playerTransform;
-
-    [SerializeField] private float spawnHeight = 10f;
+    [Header("Settings")]
     [SerializeField] private float horizontalRangeToInstanciate = 2.5f;
     [SerializeField] private float distanceFromPlayerToSpawnY = -2f;
 
-    private float lastSpawnPositionY;
+    private float positionToSpawnInY = 10f;
 
-    private float difficultyIncreaseHeight = 100f;
+    private float heightGoalToIncreaseDifficulty = 100f;
     private float hardesEnemiesPropability = 0;
 
     void Update() {
@@ -34,28 +33,29 @@ public class EnemiesSpawner : MonoBehaviour {
 
         UpdateDifficultyPropability();
     }
-
-    private void UpdateDifficultyPropability() {
-
-        if (playerTransform.position.y >= difficultyIncreaseHeight) {
-            difficultyIncreaseHeight += 50f;
-            hardesEnemiesPropability += 5f;
-        }
-    }
+    private float lastSpawnPositionY;
 
     private bool CanSpawn() {
         return (playerTransform.position.y - lastSpawnPositionY) >= distanceFromPlayerToSpawnY;
     }
 
+    private void UpdateDifficultyPropability() {
+
+        if (playerTransform.position.y >= heightGoalToIncreaseDifficulty) {
+            heightGoalToIncreaseDifficulty += 50f;
+            hardesEnemiesPropability += 5f;
+        }
+    }
+
     private void SpawnEnemy(GameObject enemyPrefab) {
 
         float randomPositionX = Random.Range(-horizontalRangeToInstanciate, horizontalRangeToInstanciate);
-        Vector3 spawnPosition = new Vector3(randomPositionX, playerTransform.position.y + spawnHeight, 0);
+        Vector3 spawnPosition = new Vector3(randomPositionX, playerTransform.position.y + positionToSpawnInY, 0);
         Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
         lastSpawnPositionY = spawnPosition.y;
 
-        if (distanceFromPlayerToSpawnY < 4f) {
-            distanceFromPlayerToSpawnY -= 0.1f;
+        if (distanceFromPlayerToSpawnY > -7f) {
+            distanceFromPlayerToSpawnY -= 0.05f;
         }
     }
 }
