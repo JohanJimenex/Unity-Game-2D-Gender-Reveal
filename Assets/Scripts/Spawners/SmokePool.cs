d404258fc032e2a52d9be5954827bc24ef2a7afc
johanjimenex <johan.jimenez.23@gmail.com>
2024-05-29 00:35:03 -0400

    refactor: Update SmokePool.cs to use Queue instead of List for object pooling

--- Assets/Scripts/Spawners/SmokePool.cs
+++ Assets/Scripts/Spawners/SmokePool.cs
@@ -7,7 +7,7 @@ public class SmokePool : MonoBehaviour {
     [SerializeField] private GameObject prefabToPool;
     [SerializeField] private int poolSize = 10;
 
-    private List<GameObject> pooledObjects;
+    private Queue<GameObject> pooledObjects;
 
     public static SmokePool instance;
 
@@ -20,33 +20,31 @@ public class SmokePool : MonoBehaviour {
             Destroy(gameObject);
         }
 
-        pooledObjects = new List<GameObject>();
+        pooledObjects = new Queue<GameObject>();
 
         for (int i = 0; i < poolSize; i++) {
             GameObject obj = Instantiate(prefabToPool);
             obj.SetActive(false);
-            pooledObjects.Add(obj);
+            pooledObjects.Enqueue(obj);
         }
     }
 
     public GameObject GetObjectFromPool() {
         if (pooledObjects.Count > 0) {
-            GameObject obj = pooledObjects[0];
-            pooledObjects.RemoveAt(0);
+            GameObject obj = pooledObjects.Dequeue();
             obj.SetActive(true);
-            // Reset any properties/components if needed
             return obj;
         }
         else {
             GameObject obj = Instantiate(prefabToPool);
-            pooledObjects.Add(obj);
+            pooledObjects.Enqueue(obj);
             return obj;
         }
     }
 
     public void ReturnObjectToPool(GameObject obj) {
         obj.SetActive(false);
-        pooledObjects.Add(obj);
+        pooledObjects.Enqueue(obj);
         // Reset any properties/components if needed
     }
 }
