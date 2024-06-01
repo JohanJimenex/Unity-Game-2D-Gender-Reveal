13bcdda750b2664baac7aba95656e7a7e218e4d2
johanjimenex <johan.jimenez.23@gmail.com>
2024-06-01 11:36:22 -0400

    refactor: Remove unused code for touch controls and dash power in PlayerMovement.cs

--- Assets/Scripts/Characters/Player/PlayerMovement.cs
+++ Assets/Scripts/Characters/Player/PlayerMovement.cs
@@ -44,11 +44,11 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetButtonDown("Jump")) {
             rb.velocity = Vector2.up * jumpForce;
         }
-        else if (Input.GetKeyDown(KeyCode.S) && isDownDashActive) {
-            playerHealthManager.SetInvencible(0.4f);
-            rb.velocity = Vector2.down * jumpForce;
-            Invoke(nameof(StopDownForce), 0.3f);
-        }
+        // else if (Input.GetKeyDown(KeyCode.S) && isDownDashActive) {
+        //     playerHealthManager.SetInvencible(0.4f);
+        //     rb.velocity = Vector2.down * jumpForce;
+        //     Invoke(nameof(StopDownForce), 0.3f);
+        // }
 
         //move to the left
         if (Input.GetKeyDown(KeyCode.A)) {
@@ -61,30 +61,45 @@ public class PlayerMovement : MonoBehaviour {
 
         // ================== Touch Controls ==================
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
-            startTouchPosition = Input.GetTouch(0).position;
-        }
+        // if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+        //     startTouchPosition = Input.GetTouch(0).position;
+        // }
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+        // if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
 
-            endTouchPosition = Input.GetTouch(0).position;
+        //     endTouchPosition = Input.GetTouch(0).position;
 
-            if (endTouchPosition.y < startTouchPosition.y && isDownDashActive) {
-                rb.velocity = Vector2.down * jumpForce;
-                Invoke(nameof(StopDownForce), 0.3f);
-            }
-            else if (endTouchPosition.y >= startTouchPosition.y) {
-                rb.velocity = Vector2.up * jumpForce;
-            }
+        //     if (endTouchPosition.y < startTouchPosition.y && isDownDashActive) {
+        //         rb.velocity = Vector2.down * jumpForce;
+        //         Invoke(nameof(StopDownForce), 0.3f);
+        //     }
+        //     else if (endTouchPosition.y >= startTouchPosition.y) {
+        //         rb.velocity = Vector2.up * jumpForce;
+        //     }
 
-            if (endTouchPosition.x + 100 < startTouchPosition.x) {
-                // rb.velocity = Vector2.left * moveForce * Time.deltaTime;
+        //     if (endTouchPosition.x + 100 < startTouchPosition.x) {
+        //         // rb.velocity = Vector2.left * moveForce * Time.deltaTime;
+        //         rb.velocity = new Vector2(-1 * moveXForce, 1 * 2);
+        //     }
+        //     else if (endTouchPosition.x - 100 > startTouchPosition.x) {
+        //         // rb.velocity = Vector2.right * moveForce * Time.deltaTime;
+        //         rb.velocity = new Vector2(1 * moveXForce, 1 * 2);
+
+        //     }
+        // }
+
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = touch.position;
+
+            if (touchPosition.x < Screen.width / 3) {
                 rb.velocity = new Vector2(-1 * moveXForce, 1 * 2);
             }
-            else if (endTouchPosition.x - 100 > startTouchPosition.x) {
-                // rb.velocity = Vector2.right * moveForce * Time.deltaTime;
+            else if (touchPosition.x > Screen.width / 3 + Screen.width / 3) {
                 rb.velocity = new Vector2(1 * moveXForce, 1 * 2);
-
+            }
+            else {
+                rb.velocity = Vector2.up * jumpForce;
             }
         }
 
@@ -103,16 +118,16 @@ public class PlayerMovement : MonoBehaviour {
         rb.velocity = Vector2.up * 3;
     }
 
-    private bool isDownDashActive = false;
+    // private bool isDownDashActive = false;
 
-    public void ActiveDownDash(int durationInSeconds) {
-        isDownDashActive = true;
-        Invoke(nameof(DeactivateDownDash), durationInSeconds);
-    }
+    // public void ActiveDownDash(int durationInSeconds) {
+    //     isDownDashActive = true;
+    //     Invoke(nameof(DeactivateDownDash), durationInSeconds);
+    // }
 
-    private void DeactivateDownDash() {
-        isDownDashActive = false;
-    }
+    // private void DeactivateDownDash() {
+    //     isDownDashActive = false;
+    // }
 
     private void EmitPlayerPositionY() {
         OnPositionYChanged?.Invoke(transform.position.y);
