a40d5731e72baa1f79ed10859ef6a82b24c6e920
johanjimenex <johan.jimenez.23@gmail.com>
2024-06-01 18:38:01 -0400

    refactor: Update settings controller to handle new touch controls and mute options

--- Assets/Scripts/Managers/SettingsController.cs
+++ Assets/Scripts/Managers/SettingsController.cs
@@ -8,22 +8,32 @@ public class SettingsController : MonoBehaviour {
     [SerializeField] private Toggle toggle;
 
     private void Start() {
-        if (gameObject.CompareTag("ToggleMusic")) {
-            toggle.SetIsOnWithoutNotify(AudioManager.instance.musicAudioSource.mute);
+
+        if (gameObject.CompareTag("ToggleControls")) {
+            bool changeControls = PlayerPrefs.GetInt("UseNewTouchControls", 1) == 1;
+            toggle.SetIsOnWithoutNotify(changeControls);
         }
-        else if (gameObject.CompareTag("ToggleSoundFx")) {
-            toggle.SetIsOnWithoutNotify(AudioManager.instance.soundsFXAudioSource.mute);
-        }
+
+    }
+
+    public void ChangeControls() {
+        PlayerMovement.instance.useNewTouchControls = !PlayerMovement.instance.useNewTouchControls;
+        PlayerAnim.instance.useNewTouchControls = !PlayerAnim.instance.useNewTouchControls;
+        toggle.SetIsOnWithoutNotify(PlayerMovement.instance.useNewTouchControls);
+        PlayerPrefs.SetInt("UseNewTouchControls", PlayerMovement.instance.useNewTouchControls ? 1 : 0);
     }
 
     public void MuteMusic() {
         AudioManager.instance.musicAudioSource.mute = !AudioManager.instance.musicAudioSource.mute;
         toggle.SetIsOnWithoutNotify(AudioManager.instance.musicAudioSource.mute);
+        PlayerPrefs.SetInt("MuteMusic", AudioManager.instance.musicAudioSource.mute ? 1 : 0);
+
     }
 
     public void MuteSoundFx() {
         AudioManager.instance.soundsFXAudioSource.mute = !AudioManager.instance.soundsFXAudioSource.mute;
         toggle.SetIsOnWithoutNotify(AudioManager.instance.soundsFXAudioSource.mute);
+        PlayerPrefs.SetInt("MuteSoundFx", AudioManager.instance.soundsFXAudioSource.mute ? 1 : 0);
     }
 
 }
